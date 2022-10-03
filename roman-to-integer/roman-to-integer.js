9a4c8d77cd0e8a5d41b85cd1c9b8060ce3c3df7d
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-10-03 19:54:50 -0300

    roman-to-integer

---
+++ roman-to-integer/roman-to-integer.js
@@ -0,0 +1,11 @@
+var romanToInt = function(s) {
+    const romanKey = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
+    
+    s = s.replace("IV","IIII").replace("IX","VIIII");
+    s = s.replace("XL","XXXX").replace("XC","LXXXX");
+    s = s.replace("CD","CCCC").replace("CM","DCCCC");
+    
+    return [...s].reduce((acc,currLetter,i,arr) => {
+        return acc + romanKey[currLetter]
+    }, 0)
+};
